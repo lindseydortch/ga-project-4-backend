fee94ceedde235a97f0a55f6729d7c2728b889ed
lindseydortch <codewithlinds@gmail.com>
2021-09-16 23:13:04 -0500

    finished profiles CRUD

--- cadere_django/settings.py
+++ cadere_django/settings.py
@@ -43,12 +43,18 @@ INSTALLED_APPS = [
     'profiles',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
